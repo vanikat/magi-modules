84087c7ae41857367ccfb1d6a16d38a367fe395d
Ms. Rucha Sanjay Vaidya <vaidyars@users.isi.deterlab.net>
2015-07-07 09:12:19 -0700

    DTN_send_rec - Agent to send and receive files using the Delay Tolerant Network

---
+++ DTN_send_recv/DTN_send_recv.py
@@ -0,0 +1,217 @@
+#!/usr/bin/env python
+
+from magi.util.agent import DispatchAgent, agentmethod
+from magi.util.processAgent import initializeProcessAgent
+
+from subprocess import Popen, PIPE
+import logging
+import sys
+
+log = logging.getLogger()
+#logging.basicConfig(filename='/users/vaidyars/dtn.log',level=log.info)
+
+from magi.util import database, config
+
+import time
+
+# The DTN_send_recv agent implementation, derived from DispatchAgent.
+class DTN_send_recv(DispatchAgent):
+    def __init__(self):
+        DispatchAgent.__init__(self)
+        self.process_s_dtnd = None
+        self.process_c_dtnd = None
+        self.process_dtncpd = None
+        self.process_dtncp = None
+	self.collection = database.getCollection(self.name)
+    
+    def setup_server(self, msg):
+        
+        configFile = "/proj/montage/exp/casestudy1/servernode_dtn.conf"
+        logFile = "/home/user/dtn/dtnd.log"
+        
+        log.info("Setting up server")
+#         return True
+        # this has to be changed to setting up a conf file on its own
+        
+        # this initialized the database required for the DTN
+        log.info("Initializing the database")
+        process = Popen(["sudo", "dtnd", "-c", configFile, "--init-db"], stdout=PIPE, stderr=PIPE)
+        returncode = process.wait()
+        stdout, stderr = process.communicate()
+        if returncode == 0:
+            log.info(stdout)
+            log.info("Database initialized")
+        else:
+            log.error(stderr)
+            log.error("Error initializing database. Returncode %d" %(returncode))
+        
+        # this starts the DTN daemon
+        log.info("Starting daemon")
+        self.process_s_dtnd = Popen(["sudo",  "dtnd", "-c", configFile, "-o", logFile], stdout=PIPE, stderr=PIPE)
+        log.info("Daemon started")
+        
+        return True    
+
+    def setup_client(self, msg):
+        # this copies the dtn.conf file later has to be changed to settig up a conf file on its own
+        
+        configFile = "/proj/montage/exp/casestudy1/clientnode_dtn.conf"
+        logFile = "/home/user/dtn/dtnd.log"
+        
+        log.info("Setting up client")
+#         return True
+        
+        # this initialized the database required for the DTN
+        log.info("Initializing the database")
+        process = Popen(["sudo", "dtnd", "-c", configFile, "--init-db"], stdout=PIPE, stderr=PIPE)
+        returncode = process.wait()
+        stdout, stderr = process.communicate()
+        if returncode == 0:
+            log.info(stdout)
+            log.info("Database initialized")
+        else:
+            log.error(stderr)
+            log.error("Error initializing database. Returncode %d" %(returncode))
+        
+        # this starts the DTN daemon
+        log.info("Starting daemon")
+        self.process_c_dtnd = Popen(["sudo",  "dtnd", "-c", configFile, "-o", logFile], stdout=PIPE, stderr=PIPE)
+        log.info("Client daemon started")
+        
+        return True    
+
+    def start_recv(self, msg):
+        log.info("Starting receive process")
+        self.process_dtncpd = Popen(["sudo", "dtncpd", "/home/user/dtn/incoming"], stdout=PIPE, stderr=PIPE)
+        log.info("Started receiving")
+        return True    
+
+    def random_send(self, msg):
+        for i in range(0, 50):
+            filename = '/tmp/' + str(i) + '.txt'
+            Popen(["sudo", "touch", filename])
+            self.start_send(filename, 'dtn://servernode.dtn')
+            #time.sleep(0.5)
+        return True  
+    
+    def start_send(self, filename, dest_dtn_eid):
+        log.info("Trying to send file %s to %s" %(filename, dest_dtn_eid))
+        process = Popen(["sudo", "dtncp", filename, dest_dtn_eid], stdout=PIPE, stderr=PIPE)
+        returncode = process.wait()
+	t = time.time()
+        out, err = process.communicate()
+        if returncode == 0:
+	    self.collection.insert({"filename" : filename, 
+                                    "send_time" : t})
+            log.info(out)
+            log.info("File: %s sent at time: %s " %(filename, str(t)))
+
+
+        else:
+            log.error(err)
+            log.error("Error sending file: %s sent at time: %s " %(filename, str(time.time())))
+        
+        
+    #    if ret_val == 0:
+    #        to_log = "\nFILE: "+filename+" sent at TIME: " + str(t) + "\n"
+    #        log.info(to_log)
+    #    else: 
+    #        to_log = "\nFILE: "+filename+" not sent to "+dest_dtn_eid+". Return Value is" + str(ret_val) + "\n"
+    #        log.info(to_log)
+    
+        return True    
+
+
+    def stop_server(self, msg):
+        log.info("Terminating the server")
+        time.sleep(2)
+        if self.process_s_dtnd:
+            self.process_s_dtnd.terminate()
+            out, err = self.process_s_dtnd.communicate()
+            returncode = self.process_s_dtnd.poll()
+            if returncode == 0:
+                log.info(out)
+                log.info("Server terminated")
+            else:
+                log.error("Error terminating server. Returncode: %d" %(returncode))
+                log.error(err)
+        else:
+            log.info("No server process running")
+        return True 
+
+    def stop_client(self, msg):
+        log.info("Terminating the client")
+        time.sleep(2)
+        if self.process_c_dtnd:
+            self.process_c_dtnd.terminate()
+            out, err = self.process_c_dtnd.communicate()
+            returncode = self.process_c_dtnd.poll()
+            if returncode == 0:
+                log.info(out)
+                log.info("Client terminated")
+            else:
+                log.error("Error terminating client. Returncode: %d" %(returncode))
+                log.error(err)
+        else:
+            log.info("No client process running")
+        return True    
+
+    def parse_output_to_database(out):
+	# format of output: "%ld,%ld,%s,%s,%d\n", recv_count, current, host, filename,(int) st.st_size
+	list_out = out.split( );
+	for s in list_out:
+		srno, time, source, filename, size = line.split(',')       # split up line around comma characters
+		self.collection.insert({"srno" : srno,
+					"recv_time" : time, 
+					"source" : source, 
+					"filename" : filename, 
+					"size" : size})
+    	return True;
+
+
+    def stop_recv(self, msg):
+        log.info("Stopping to receive")
+        time.sleep(2)
+        if self.process_dtncpd:
+            self.process_dtncpd.terminate()
+            out, err = self.process_dtncpd.communicate()
+            returncode = self.process_dtncpd.poll()
+            if returncode == 0:
+                log.info("Receive process terminated")
+                log.info("Received Files:")
+                log.info(out)
+		parse_output_to_database(out)
+            else:
+                log.error("Error terminating receive process. Returncode: %d" %(returncode))
+                log.error(err)
+            log.info("Received Files:")
+            log.info(out)
+        else:
+            log.info("No receive process running")
+        log.info("All done!")
+        return True
+
+    
+    def close_log(self, msg):
+        log.info("cleanup done")
+    #    logging.shutdown()
+        return True  
+        
+
+# the getAgent() method must be defined somewhere for all agents.
+# The Magi daemon invokes this method to get a reference to an
+# agent. It uses this reference to run and interact with an agent
+# instance.
+def getAgent():
+    agent = DTN_send_recv()
+    return agent
+
+# In case the agent is run as a separate process, we need to
+# create an instance of the agent, initialize the required
+# parameters based on the received arguments, and then call the
+# run method defined in DispatchAgent.
+if __name__ == "__main__":
+    agent = DTN_send_recv()
+    kwargs = initializeProcessAgent(agent, sys.argv)
+    agent.setConfiguration(None, **kwargs)
+    agent.run()
---
+++ DTN_send_recv/final_dtn_install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#bootstraping MAGI
+sudo python /share/magi/dev/magi_bootstrap.py -p /share/magi/dev/
+
+#installing dependencies for DTN
+sudo apt-get -y install libxerces-c2-dev
+sudo apt-get -y install libxerces-c28
+sudo apt-get -y install libxerces-c3.1
+sudo apt-get -y install libxerces2-java
+sudo /share/magi/dev/source/berkely-db.install /tmp /share/magi/dev/source
+sudo /share/magi/dev/source/tcl-old.install /tmp /share/magi/dev/source
+sudo /share/magi/dev/source/oasys-old.install /tmp /share/magi/dev/source
+
+#installing DTN
+sudo /share/magi/dev/source/dtn.install /tmp /share/magi/dev/source
+
+short_hostname="$(echo $HOSTNAME| cut -d"." -f1)"
+configfile="/proj/montage/modules/DTN_send_recv/"$short_hostname"_dtn.conf"
+
+#instantiating DTN
+sudo dtnd -c $configfile --init-db
+sudo dtnd -c $configfile -o /home/user/dtn/dtnd.log -d
