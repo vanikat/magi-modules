e8528386cf1136551f974fe9a5edf2d91bf61b9b
Rafael Ning <rafaelning@gmail.com>
2016-07-07 15:02:05 -0700

    Create runMagi.sh

---
+++ ShellScript/runMagi.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Start magi_orchestrator.py
+echo ~-~-~Running magi_orchestrator.py~-~-~
+# CHANGE THE BELOW PATH
+cd ~/magi-modules/FileCreator
+magi_orchestrator.py -p montage -e rningCalc -f FileCreator.aal
