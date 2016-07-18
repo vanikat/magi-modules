3d04215b0e28b0e6e436bec8364524489ce3486c
Rafael Ning <rafaelning@gmail.com>
2016-07-18 14:47:08 -0700

    Update Building.py

--- SmartGridSchool/Building.py
+++ SmartGridSchool/Building.py
@@ -15,3 +15,18 @@ class Building(): # difference between DispatchAgent and NonBlockingDispatchAgen
   def __init__(self):
     DispatchAgent.__init__(self)
     
+
+# getAgent() method must be defined somewhere for all agents.
+# Magi daemon invokes method to get reference to agent. Uses reference to run and interact with agent instance.
+def getAgent(**kwargs):
+  agent = Building()
+  agent.setConfiguration(None, **kwargs)
+  return agent
+
+# In case agent run as separate process, need to create instance of agent, initialize required
+# parameters based on received arguments, and call run method defined in DispatchAgent.
+if __name__ == "__main__":
+    agent = Building()
+    initializeProcessAgent(agent, sys.argv)
+    agent.setConfiguration(None, **kwargs)
+    agent.run()
