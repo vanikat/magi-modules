257905b5509ff828b5e6d2371ac529e860d701d7
Rafael Ning <rafaelning@gmail.com>
2016-07-18 16:00:58 -0700

    Update codeBits.py

--- SmartGridSchool/codeBits.py
+++ SmartGridSchool/codeBits.py
@@ -1,13 +1,21 @@
 # This code for parsing parameters.conf and creating dictionary for each building
 # To be run when each building's function is called by orchestrator (as opposed to args being passed by orchestrator)
 def setConfiguration(self, msg, **kwargs):
+  parameters = open("parameters.conf").readlines();
+  
+  
+  
+  
+  
   with open("parameters.conf", r) as parameters:
     from magi.testbed import testbed
     self.hostname = testbed.nodename # should be b-0 to b-21
     for line in parameters:
-      if "day" in line:
+      if self.hostname in line:
+        
+      elif "day" in line:
         self.day = line[len("day:"):]
-      if "solarIrradiance" in line:
+      elif "solarIrradiance" in line:
         self.solarIrradiance = line[len("solarIrradiance:"):]
-      if "panelEff" in line:
+      elif "panelEff" in line:
         self.panelEff = line[len("panelEff:"):]
