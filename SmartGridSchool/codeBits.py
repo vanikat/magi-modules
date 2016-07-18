3b81808a8f3c1e5162fd4c794bf584845f4fb170
Rafael Ning <rafaelning@gmail.com>
2016-07-18 15:02:16 -0700

    Update codeBits.py

--- SmartGridSchool/codeBits.py
+++ SmartGridSchool/codeBits.py
@@ -1,4 +1,8 @@
 # This code for parsing parameters.conf and creating dictionary for each building
+# To be run when each building's function is called by orchestrator (as opposed to args being passed by orchestrator)
 def setConfiguration(self, msg, **kwargs):
-  
-  with open(
+  with open("parameters.conf", r) as parameters:
+    from magi.testbed import testbed
+    self.hostname = testbed.nodename # should be b-0 to b-21
+    for line in parameters:
+      
