d1821f581962289a59aa8f10e726b5eab0e75da8
Rafael Ning <rafaelning@gmail.com>
2016-07-21 15:56:27 -0700

    Update Building.py

--- SmartGridSchool/Building.py
+++ SmartGridSchool/Building.py
@@ -13,15 +13,34 @@ from magi.util.agent import DispatchAgent, agentmethod
 from magi.util.processAgent import initializeProcessAgent
 import yaml
 
+from CommClient import ClientCommService
+
+log = logging.getLogger(__name__)
+
 class Building(): # difference between DispatchAgent and NonBlockingDispatchAgent?
     def __init__(self):
-    DispatchAgent.__init__(self)
+        DispatchAgent.__init__(self)
+        
+        #Default arguments for server and clientId:
+        self.server = 'localhost'
+        self.clientId = None
     
-    # This code for parsing parameters.conf and creating dictionary for each building
-    # To be run when each building's function is called by orchestrator (as opposed to args being passed by orchestrator)
+    # This code for parsing parameters.conf
     def setConfiguration(self, msg, **kwargs):
         DispatchAgent.setConfiguration(self, msg, **kwargs)
         
+        '''From simple_client > client.py'''
+        from magi.testbed import testbed
+        if self.clientId == None:
+            self.hostname = testbed.nodename # should be b-0 to b-21
+            log.info("Hostname: %s", self.hostname)
+            self.clientId = self.hostname
+        
+        # The clientId can be set programmatically to append the hostname .
+        self.setClientid() 
+        self.commClient = None
+        
+        '''Parsing for Parameters'''
         # create list of strings
         with open("parameters.conf", r) as parameters:
             self.paramsList = paramsFile.read().splitlines()
@@ -32,8 +51,8 @@ class Building(): # difference between DispatchAgent and NonBlockingDispatchAgen
         self.panelEff = paramList[2][len("panelEff:"):]
         
         # define the parameters unique to the building
-        from magi.testbed import testbed 
-        self.hostname = testbed.nodename # should be b-0 to b-21
+        ## from magi.testbed import testbed 
+        ## self.hostname = testbed.nodename # should be b-0 to b-21
         index = self.paramsList.index(self.hostname)
         self.area = paramList[index+1][len("area:"):] 
         self.panelArea = paramList[index+2][len("panelArea:"):]
@@ -42,7 +61,41 @@ class Building(): # difference between DispatchAgent and NonBlockingDispatchAgen
         self.outlets = paramList[index+5][len("outlets:"):]
         self.aapplianceDraw = paramList[index+6][len("applianceDraw:"):]
         self.tempAC = paramList[index+7][len("tempAC:"):]
-  
+    
+    # From simple_client > client.py
+    def setClientid(self):
+    # The method is called in the setConfiguration to set a unique clientID  for the client in a group 
+    # If there is only one client per host, then the clientId can be the hostname 
+    # but if there are more than one clients per host, we will need to add a random int to it... 
+        return
+    
+    # From simple_client > client.py
+    @agentmethod()
+    def startclient(self, msg):
+        self.commClient = ClientCommService(self.clientId)
+        self.commClient.initCommClient(self.server, self.requestHandler)
+    
+    # From simple_client > client.py
+    @agentmethod()
+    def stopclient(self, msg):
+        DispatchAgent.stop(self, msg)
+        if self.commClient:
+            self.commClient.stop()
+    
+    # From simple_client > client.py
+    def requestHandler(self, msgData):
+        log.info("RequestHandler: %s", msgData)
+        
+        dst = msgData['dst']
+        if dst != self.hostname:
+            log.error("Message sent to incorrect destination.")
+            return
+        
+        src= msgData['src']
+        string = msgData['string']
+        
+        log.info("src and string: %s %s", src, string)
+    
     # electricity generation 
     def generation(self, msg):
         
@@ -62,6 +115,6 @@ def getAgent(**kwargs):
 # parameters based on received arguments, and call run method defined in DispatchAgent.
 if __name__ == "__main__":
     agent = Building()
-    initializeProcessAgent(agent, sys.argv)
+    kwargs = initializeProcessAgent(agent, sys.argv)
     agent.setConfiguration(None, **kwargs)
     agent.run()
