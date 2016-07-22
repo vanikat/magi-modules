5e7d91a2187bb74dcd66e2a68d7b42805f5d56d4
Rafael Ning <rafaelning@gmail.com>
2016-07-22 15:33:30 -0700

    Update Topology.tcl

--- SmartGridSchool/Topology.tcl
+++ SmartGridSchool/Topology.tcl
@@ -9,7 +9,6 @@ tb-set-node-startcmd $control "$magi_start"
 # Buildings (21 clients total)
 set A 20 
 set clanstr ""
-
 for {set i 0 } {$i <= $A } { incr i } {  
         set b($i) [$ns node]
         tb-set-node-startcmd $b($i) "$magi_start" 
@@ -17,20 +16,28 @@ for {set i 0 } {$i <= $A } { incr i } {
 } 
 
 # Server (1 server total)
-set A 0
-set slanstr ""
+set server [$ns node]
+tb-set-node-startcmd $server "$magi_start" 
 
-for {set i 0 } {$i <= $A } { incr i } {  
-        set server($i) [$ns node]
-        tb-set-node-startcmd $server($i) "$magi_start" 
-        append slanstr "$server($i) "
+#set A 0
+#set slanstr ""
+#for {set i 0 } {$i <= $A } { incr i } {  
+#        set server($i) [$ns node]
+#        tb-set-node-startcmd $server($i) "$magi_start" 
+#        append slanstr "$server($i) "
+#}
+
+# Setting links from each building client to the server
+set A 20
+for {set i 0 } {$i <= $A } { incr i } {
+        set link($i) [$ns duplex-link $b($i) $server 100Mbps 0ms DropTail]
 }
 
-set router [$ns node] 
-tb-set-node-startcmd $router "$magi_start"
+#set router [$ns node] 
+#tb-set-node-startcmd $router "$magi_start"
 
-set lanC [$ns make-lan "router $clanstr" 10Mb 0ms]
-set lanS [$ns make-lan "router $slanstr" 10Mb 0ms]
+#set lanC [$ns make-lan "router $clanstr" 10Mb 0ms]
+#set lanS [$ns make-lan "router $slanstr" 10Mb 0ms]
 
 $ns rtproto Static
 $ns run
