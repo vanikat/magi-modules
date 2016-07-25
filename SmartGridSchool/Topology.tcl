dffdfc413c06eb12211e1b79c22923516e1ab760
Rafael Ning <rafaelning@gmail.com>
2016-07-25 10:54:05 -0700

    Update Topology.tcl

--- SmartGridSchool/Topology.tcl
+++ SmartGridSchool/Topology.tcl
@@ -8,11 +8,13 @@ tb-set-node-startcmd $control "$magi_start"
 tb-set-node-os $control Ubuntu1204-64-STD
 
 # Buildings (21 clients total)
+set lanstr ""
 set A 20 
 for {set i 0 } {$i <= $A } { incr i } {  
         set b($i) [$ns node]
         tb-set-node-startcmd $b($i) "$magi_start" 
         tb-set-node-os $b($i) Ubuntu1204-64-STD
+        append lanstr "$node($i)"
 } 
 
 # Server (1 server total)
@@ -20,11 +22,14 @@ set server [$ns node]
 tb-set-node-startcmd $server "$magi_start" 
 tb-set-node-os $server Ubuntu1204-64-STD
 
-# Setting links from each building client to the server
-set A 20
-for {set i 0 } {$i <= $A } { incr i } {
-        set link($i) [$ns duplex-link $b($i) $server 100Mbps 0ms DropTail]
-}
+# create the LAN topology
+set lan0 [$ns make-lan "$lanstr" 100Mb 0ms]
+
+# Setting links from each building client to the server (5 LINKS MAX)
+#set A 20
+#for {set i 0 } {$i <= $A } { incr i } {
+#        set link($i) [$ns duplex-link $b($i) $server 100Mbps 0ms DropTail]
+#}
 
 $ns rtproto Static
 $ns run
